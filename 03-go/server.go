c82e6f30e9574400218f9bd5b76856d4ffe766f8
Rafael Batista <rbtista383@gmail.com>
2023-11-07 06:01:43 -0300

    v5

--- 03-go/server.go
+++ 03-go/server.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/", Hello)
-	fmt.Println("Servidor Go executando na porta 8000...")
 	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "<h1>Rafael Batista</h1>")
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password: %s", user, password)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "My Family: %s.", string(data))
 }
